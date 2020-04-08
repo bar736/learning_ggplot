74bd0393eb461540ba097ba15577fa260ecaa272
Barrero Sanchez <bar736@csiro.au>
2020-04-08 10:53:31 +1000

    second day of ggplot learning 8-4-2020

--- scripts/learning_ggplot.R
+++ scripts/learning_ggplot.R
@@ -126,7 +126,7 @@ ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
 a_countries <- filter(gapminder, str_starts(country, "A"))
 
 ggplot(data = a_countries, aes(x = year, y = lifeExp, colour = continent, group = country)) +
-  geom_line() + 
+  geom_line(colour = "blue") + 
   facet_wrap( ~ country)
 
 
@@ -165,4 +165,98 @@ ggplot(
   #<SCALE FUNCTION> +
   #<FACET FUNCTION> +
   #...
- 
+
+
+#getting the plots ready for publication Wednesday 8-4-2020
+
+rough_plot <- ggplot(data = a_countries, aes(x = year, y = lifeExp, colour = continent, group = country)) +
+  geom_line() + 
+  facet_wrap( ~ country)    #this saves the plot as an object
+
+
+rough_plot +    #labs: this modify whatever you have mapped as aesthetics
+  labs(title = "Growth in life expectancy for 'A' countries",
+       caption = 'Data source: Gap minder',
+       x = "Year",
+       y = "Life Expectancy",
+       colour = "Continent") +  
+  
+  #themes: to change the aspect of the plot
+  theme_bw()  +  
+  theme(
+    panel.grid.minor = element_blank(),
+    plot.title = element_text(face = "bold")
+  )
+
+#challenge 2
+#What would you pass to the theme() function to make the following changes?
+#1) Remove the grey boxes behind the country names (controlled by the strip.background parameter).
+#2) Increase the size of the major gridlines (panel.grid.major) to 1 (need to use element_line())
+#3) Change the axis titles to be shrunk to size 10 and coloured blue
+#4) Position the legend at the bottom of the plot.
+
+rough_plot +    
+  labs(title = "Growth in life expectancy for 'A' countries",
+       caption = 'Data source: Gap minder',
+       x = "Year",
+       y = "Life Expectancy",
+       colour = "Continent") +  
+  theme_bw()  +  
+  theme(
+    panel.grid.minor = element_blank(),
+    plot.title = element_text(face = "bold"),
+    
+    strip.background = element_blank(),
+    panel.grid.major = element_line(size = 1),
+    axis.title = element_text(size = 10, colour = "blue"),
+    legend.position = "bottom",
+  )
+
+#Exporting the plot. Make a new object and save it in the results folder
+lifeExp_plot <- rough_plot +    
+  labs(title = "Growth in life expectancy for 'A' countries",
+       caption = 'Data source: Gap minder',
+       x = "Year",
+       y = "Life Expectancy",
+       colour = "Continent") +  
+  theme_bw()  +  
+  theme(
+    panel.grid.minor = element_blank(),
+    plot.title = element_text(face = "bold"),
+    
+    strip.background = element_blank(),
+    panel.grid.major = element_line(size = 1),
+    axis.title = element_text(size = 10, colour = "blue"),
+    legend.position = "bottom",
+  )
+
+lifeExp_plot #we have created a new object and we we save it as png or jpg, etc:
+
+ggsave(filename = "results/life.Exp.jpg", plot = lifeExp_plot,
+       width = 18, height = 12, dpi = 300, units = "cm")
+
+
+#Multi-panel figures using cowplot package
+
+install.packages("cowplot")
+library(cowplot)
+
+# Making some plots
+plot1 <- ggplot(gapminder, aes(x = gdpPercap, y = lifeExp)) + geom_point()
+plot2 <- ggplot(gapminder, aes(x = continent, y = lifeExp)) + geom_boxplot()
+plot3 <- ggplot(gapminder, aes(x = gdpPercap, y = pop)) + geom_point()
+plot4 <- ggplot(gapminder, aes(x = lifeExp, y = pop)) + geom_point()
+
+# Combining them into one using the cowplot funtion "plot_grid" 
+#the default settings gives the same area to all panels
+plot_grid(plot1, plot2, plot3, plot4)
+
+#the rel_heights argument modifies the size of the panels 
+plot_grid(plot1, plot2, plot3, plot4, rel_heights = c(1, 3),
+          rel_widths = c(1, 2))
+
+#The subfigures can be labelled with the labels argument. 
+#You can either provide the exact labels or use "AUTO" 
+#for uppercase letters as labels, and "auto" for lowercase letters.
+
+plot_grid(plot1, plot2, plot3, plot4, labels = "AUTO")
